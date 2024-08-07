b4ae441c828113af7f69a82717eaeddb7c8b8b99
jar-us <suraj.sharma3963@gmail.com>
2024-08-07 11:40:55 +0000

    7 Aug 2024, 17:10

--- k-objects.sh
+++ k-objects.sh
@@ -9,9 +9,9 @@ echo "--------------------------------------------------------------------------
 
 # Iterate over each namespace and print pod details
 for ns in $namespaces; do
-  # Get pods and extract desired information
-  kubectl get pods -n "$ns" -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{range .status.containerStatuses[*]}{.ready}{"\t"}{.name}{"\t"}{end}{.status.phase}{"\n"}{end}' |
-    while read -r namespace podname ready containername status; do
-      printf "%-20s %-40s %-10s %-40s %-10s\n" "$namespace" "$podname" "$ready" "$containername" "$status"
-    done
+    # Get pods and extract desired information
+    kubectl get pods -n "$ns" -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{range .status.containerStatuses[*]}{.ready}{"\t"}{.name}{"\t"}{end}{.status.phase}{"\n"}{end}' |
+        while read -r namespace podname ready containername status; do
+            printf "%-20s %-40s %-10s %-40s %-10s\n" "$namespace" "$podname" "$ready" "$containername" "$status"
+        done
 done
