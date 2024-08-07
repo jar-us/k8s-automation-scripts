c5e4254230efa68fcce9cc71b40081ef2f018119
jar-us <suraj.sharma3963@gmail.com>
2024-08-07 12:07:57 +0000

    7 Aug 2024, 17:37

--- k-objects.sh
+++ k-objects.sh
@@ -12,14 +12,14 @@ file_name="${app_name}-${current_date_time}.csv"
 # Print header for the table and redirect to the CSV file
 printf "NAMESPACE,POD_NAME,READY,CONTAINER_NAME,STATUS\n" > "$file_name"
 
-# Get all namespaces
-namespaces=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}')
+# Get all namespaces, excluding kube-node-lease, kube-public, kube-system
+namespaces=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | grep -Ev '^(kube-node-lease|kube-public|kube-system)$')
 
 # Iterate over each namespace and print pod details
 for ns in $namespaces; do
   # Get pods and extract desired information
-  kubectl get pods -n "$ns" -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{range .status.containerStatuses[*]}{.ready}{"\t"}{.name}{"\t"}{end}{.status.phase}{"\n"}{end}' |
-    while read -r namespace podname ready containername status; do
+  kubectl get pods -n "$ns" -o jsonpath='{range .items[*]}{.metadata.namespace}{"\t"}{.metadata.name}{"\t"}{.status.phase}{"\t"}{range .status.containerStatuses[*]}{.ready}{"\t"}{.name}{"\n"}{end}{end}' |
+    while IFS=$'\t' read -r namespace podname status ready containername; do
       # Append each row to the CSV file
       printf "%s,%s,%s,%s,%s\n" "$namespace" "$podname" "$ready" "$containername" "$status" >> "$file_name"
     done
